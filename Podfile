2402069cf8b3586dfff28b5ee9ea79199b796779
tatsumi kentaro <tatsuken4290@gmail.com>
2018-03-12 00:30:19 +0900

    introductionを設定

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Swift4Quiz' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Instructions', '~> 1.0.0'
+  pod 'Gecco'
+
+  # Pods for Swift4Quiz
+
+end
--- Swift4Quiz/ViewController.swift
+++ Swift4Quiz/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import Instructions
+class ViewController: UIViewController,CoachMarksControllerDataSource,CoachMarksControllerDelegate  {
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var startButton: UIButton!
+    let coachMarksController = CoachMarksController()
+    let coachMarksController1 = CoachMarksController()
+    let pointOfInterest = UIView()
+    let pointOfInterest1 = UIView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.coachMarksController.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.coachMarksController.start(on: self)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.coachMarksController.stop(immediately: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func coachMarksController(_ coachMarksController: CoachMarksController, coachMarkViewsAt index: Int, madeFrom coachMark: CoachMark) -> (bodyView: CoachMarkBodyView, arrowView: CoachMarkArrowView?) {
+        
+        let coachViews = coachMarksController.helper.makeDefaultCoachViews(withArrow: true, arrowOrientation: coachMark.arrowOrientation)
+        
+        coachViews.bodyView.hintLabel.text = "これは説明文だよ！！！！！！！！"
+        coachViews.bodyView.nextLabel.text = "Ok!"
+        let coachViews1 = coachMarksController.helper.makeDefaultCoachViews(withArrow: true, arrowOrientation: coachMark.arrowOrientation)
+        coachViews1.bodyView.hintLabel.text = "個々のボタンを押してゲームを始めよう。君の冒険を待っているよ"
+        coachViews1.bodyView.nextLabel.text = "Ok!"
+        let coachArray = [coachViews,coachViews1]
+        return (bodyView: coachArray[index].bodyView, arrowView: coachArray[index].arrowView)
+        
+    }
+    
+    func coachMarksController(_ coachMarksController: CoachMarksController, coachMarkAt index: Int) -> CoachMark {
+        pointOfInterest.frame = CGRect(x: textView.frame.origin.x, y: textView.frame.origin.y, width: textView.frame.width, height: textView.frame.height)
+        pointOfInterest1.frame = CGRect(x: startButton.frame.origin.x, y: startButton.frame.origin.y, width: startButton.frame.width, height: startButton.frame.height)
+        
+        let posArray = [pointOfInterest,pointOfInterest1]
+        return coachMarksController.helper.makeCoachMark(for:posArray[index])
+    }
+    
+    func numberOfCoachMarks(for coachMarksController: CoachMarksController) -> Int {
+        return 2
+    }
+    
+    
 }
 
